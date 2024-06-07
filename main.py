5520790c468daf195e4e6599cdc7791ecb2bef74
sadwave <orangey720@gmail.com>
2024-06-07 17:40:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands, tasks
 import os
 import uuid
+import asyncio
 
 # Load the Discord bot token from environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,20 +18,43 @@ reminders = {}
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
-    # Start the reminder loop
-    reminder_loop.start()
 
-@bot.command(name='remind', help='Set a daily reminder. Usage: !remind <message>')
-async def remind(ctx, *, reminder: str):
+@bot.command(name='remind', help='Set a reminder. Usage: !remind <time> <message>\nTime examples: 1s, 1m, 1h, 1d')
+async def remind(ctx, time: str, *, reminder: str):
     user_id = ctx.author.id
     reminder_id = str(uuid.uuid4())
-    
-    if user_id not in reminders:
-        reminders[user_id] = {}
 
-    reminders[user_id][reminder_id] = reminder
-    reminder_message = f'{ctx.author.mention}, your reminder has been set. Your Reminder ID is: `{reminder_id}`'
-    await ctx.send(reminder_message)
+    # Parse time argument
+    multiplier = 1
+    if time[-1] == 's':
+        multiplier = 1
+    elif time[-1] == 'm':
+        multiplier = 60
+    elif time[-1] == 'h':
+        multiplier = 3600
+    elif time[-1] == 'd':
+        multiplier = 86400
+    else:
+        await ctx.send(f'{ctx.author.mention}, invalid time format. Please use s (seconds), m (minutes), h (hours), or d (days).')
+        return
+    
+    try:
+        duration = int(time[:-1]) * multiplier
+    except ValueError:
+        await ctx.send(f'{ctx.author.mention}, invalid time format. Please enter a valid number for the duration.')
+        return
+    
+    if duration < 1 or duration > 604800:  # Check if duration is within 1 second to 7 days
+        await ctx.send(f'{ctx.author.mention}, please choose a time between 1 second and 7 days.')
+        return
+    
+    await ctx.send(f'{ctx.author.mention}, your reminder has been set. Reminder ID: `{reminder_id}`')
+
+    # Schedule the reminder
+    await asyncio.sleep(duration)
+    user = bot.get_user(user_id)
+    if user:
+        await user.send(f'{user.mention}, here is your reminder: {reminder}\nReminder ID: `{reminder_id}`')
 
 @bot.command(name='cancelreminder', help='Cancel a reminder by ID. Usage: !cancelreminder <reminder_id>')
 async def cancelreminder(ctx, reminder_id: str):
@@ -42,19 +66,6 @@ async def cancelreminder(ctx, reminder_id: str):
         await ctx.send(f'{ctx.author.mention}, your reminder with ID `{reminder_id}` has been canceled.')
     else:
         await ctx.send(f'{ctx.author.mention}, no reminder found with ID `{reminder_id}`.')
-
-@tasks.loop(hours=24)
-async def reminder_loop():
-    for user_id, user_reminders in reminders.items():
-        for reminder_id, reminder in user_reminders.items():
-            user = bot.get_user(user_id)
-            if user:
-                reminder_message = f'Here is your daily reminder: {reminder}\nReminder ID: `{reminder_id}`'
-                await user.send(f'{user.mention}, {reminder_message}')
-
-@reminder_loop.before_loop
-async def before_reminder_loop():
-    await bot.wait_until_ready()
 
 # Run the bot
 bot.run(DISCORD_TOKEN)
