e63b294234e65fdbb8c8f96dd4bb2cc26180b7e3
sadwave <orangey720@gmail.com>
2024-06-08 19:06:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from discord.ext import commands
+import random
 
 # Load the Discord bot token from environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,16 +14,22 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Load reminders
 from reminders import reminder, cancelreminder
 
+# Load guessing game
+from guess import guess_command, reset_random_number
+
 # Load utilities
 from utils import setup_bot
 
-@bot.command(description="Help for commands") # this decorator makes a slash command
+@bot.command(description="Help for commands")  # this decorator makes a slash command
 async def bothelp(ctx):
-        embed = discord.Embed(title="Help", description="List of available commands:", color=discord.Color.blue())
-        embed.add_field(name="!reminder <time> <message>", value="Set a reminder. Time examples: 1s, 1m, 1h, 1d", inline=False)
-        embed.add_field(name="!cancelreminder <reminder_id>", value="Cancel a reminder by ID.", inline=False)
-        await ctx.send(embed=embed)
+    embed = discord.Embed(title="Help", description="List of available commands:", color=discord.Color.blue())
+    embed.add_field(name="!reminder <time> <message>", value="Set a reminder. Time examples: 1s, 1m, 1h, 1d", inline=False)
+    embed.add_field(name="!cancelreminder <reminder_id>", value="Cancel a reminder by ID.", inline=False)
+    embed.add_field(name="!guess <number>", value="Guess a random number between 1 and 100.", inline=False)
+    await ctx.send(embed=embed)
 
+# Initialize random number for guessing game
+reset_random_number()
 
 # Run the bot
 setup_bot(bot)
