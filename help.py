cc70d615aba2e0639ca4e329e0efadcde3495a66
sadwave <orangey720@gmail.com>
2024-06-08 17:36:01 -0400

    Update help.py

--- help.py
+++ help.py
@@ -4,14 +4,14 @@ from discord.ext import commands
 class HelpCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(name="help", help="Show this help message")
-    async def help_command(self, ctx):
+        
+@bot.command(description="Sends the bot's latency.") 
+    async def help(ctx)
         embed = discord.Embed(title="Help", description="List of available commands:", color=discord.Color.blue())
         embed.add_field(name="!reminder <time> <message>", value="Set a reminder. Time examples: 1s, 1m, 1h, 1d", inline=False)
         embed.add_field(name="!cancelreminder <reminder_id>", value="Cancel a reminder by ID.", inline=False)
         await ctx.send(embed=embed)
 
 async def setup_help(bot):
-    help_command = HelpCommand()
+    help = HelpCommand()
     bot.add_command(help_command)
