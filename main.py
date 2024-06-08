9dd9fdd34988283307d0bdf299639aad40daf2c5
sadwave <orangey720@gmail.com>
2024-06-08 17:46:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import os
 import discord
 from discord.ext import commands
-from discord_slash import SlashCommand
-from utils import setup_bot
 from help import HelpCommand, setup_help
 
 # Load the Discord bot token from environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Initialize the bot with the command prefix "!"
+# Initialize the bot
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-slash = SlashCommand(bot)
 
-# Setup commands
-setup_bot(bot)
+# Setup help command
 setup_help(bot)
 
 # Run the bot
