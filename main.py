87c56da479939810cdd4561d27e176f85c8478e6
sadwave <orangey720@gmail.com>
2024-06-08 17:09:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-from reminders import reminder, cancelreminder
-from utils import setup_bot
 
 # Load the Discord bot token from environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,12 +10,20 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Setup bot commands from other modules
-setup_bot(bot)
+# Import commands from the other modules
+from reminders import reminder, cancelreminder, setup_reminders
+from help import setup_help
+
+# Register commands
+setup_reminders(bot)
+setup_help(bot)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
+    # Register slash commands
+    await bot.tree.sync()
+    print(f'Synced {len(bot.tree.get_commands())} commands.')
 
 # Run the bot
 bot.run(DISCORD_TOKEN)
