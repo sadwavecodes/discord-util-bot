19339797a603968f96ac4475074e9dbaed4114ab
sadwave <orangey720@gmail.com>
2024-06-08 17:49:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import discord
 from discord.ext import commands
-from help import HelpCommand, setup_help
 
 # Load the Discord bot token from environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,8 +10,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Setup help command
-setup_help(bot)
 
 # Run the bot
 bot.run(DISCORD_TOKEN)
