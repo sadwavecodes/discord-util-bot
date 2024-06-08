6ca71a346af48cdfb61b54a3e7b97b1d35dae6dc
sadwave <orangey720@gmail.com>
2024-06-08 17:11:17 -0400

    Update help.py

--- help.py
+++ help.py
@@ -1,12 +1,11 @@
 import discord
 from discord.ext import commands
 
-def setup_help(bot):
-    @bot.slash_command(name='help', description='Show all available commands')
-    async def help(ctx):
-        embed = discord.Embed(title="Help", description="List of available commands", color=0x00ff00)
-        embed.add_field(name="!ping", value="Ping the bot to check if it's online", inline=False)
-        embed.add_field(name="!reminder", value="Set a reminder. Usage: !reminder <time> <message>", inline=False)
-        embed.add_field(name="!cancelreminder", value="Cancel a reminder by ID. Usage: !cancelreminder <reminder_id>", inline=False)
+async def help(ctx):
+    embed = discord.Embed(title="Help", description="List of available commands:", color=discord.Color.blue())
+    embed.add_field(name="!reminder <time> <message>", value="Set a reminder. Time examples: 1s, 1m, 1h, 1d", inline=False)
+    embed.add_field(name="!cancelreminder <reminder_id>", value="Cancel a reminder by ID.", inline=False)
+    await ctx.send(embed=embed)
 
-        await ctx.send(embed=embed)
+def setup_help(bot):
+    bot.add_command(commands.Command(help, name='help', help='Show this help message.'))
