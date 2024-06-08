6578d16716fbe2f52f4bf2d920e50114065dc631
sadwave <orangey720@gmail.com>
2024-06-08 18:02:00 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,10 +2,10 @@ from reminders import reminder, cancelreminder
 
 def setup_bot(bot):
     # Commands setup
-    @bot.command(name='reminder', help='Set a reminder. Usage: !reminder <time> <message>\nTime examples: 1s, 1m, 1h, 1d')
+    @bot.command(name='reminder')
     async def reminder_command(ctx, time: str, *, reminder_text: str):
         await reminder(ctx, time, reminder_text=reminder_text)
 
-    @bot.command(name='cancelreminder', help='Cancel a reminder by ID. Usage: !cancelreminder <reminder_id>')
+    @bot.command(name='cancelreminder')
     async def cancelreminder_command(ctx, reminder_id: int):
         await cancelreminder(ctx, reminder_id)
