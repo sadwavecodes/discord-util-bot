7aac48e9d690b6b2186e248db23b6a5862da8915
sadwave <orangey720@gmail.com>
2024-06-08 13:53:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 import os
-import uuid
 import asyncio
 from datetime import datetime, timedelta
 
@@ -15,6 +14,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Dictionary to store reminders
 reminders = {}
+reminder_counter = 0
 
 @bot.event
 async def on_ready():
@@ -22,6 +22,7 @@ async def on_ready():
 
 @bot.command(name='reminder', help='Set a reminder. Usage: !reminder <time> <message>\nTime examples: 1s, 1m, 1h, 1d')
 async def reminder(ctx, time: str, *, reminder_text: str):
+    global reminder_counter
     user_id = ctx.author.id
 
     # Parse time argument
@@ -40,12 +41,10 @@ async def reminder(ctx, time: str, *, reminder_text: str):
         await ctx.send(f'{ctx.author.mention}, please choose a time between 1 second and 7 days.')
         return
     
-    reminder_id = str(uuid.uuid4())
-    if reminder_id in reminders:
-        await ctx.send(f'{ctx.author.mention}, a reminder with the same ID is already set.')
-        return
+    reminder_id = reminder_counter
+    reminder_counter += 1
 
-    await ctx.send(f'{ctx.author.mention}, your reminder has been set. Reminder ID: `{reminder_id}`')
+    await ctx.send(f'{ctx.author.mention}, your reminder has been set. Reminder ID: [**{reminder_id}**](https://dummy-url/{reminder_id})')
 
     # Schedule the reminder
     reminder_time = datetime.now() + timedelta(seconds=duration)
@@ -59,12 +58,12 @@ async def reminder(ctx, time: str, *, reminder_text: str):
         del reminders[reminder_id]
 
 @bot.command(name='cancelreminder', help='Cancel a reminder by ID. Usage: !cancelreminder <reminder_id>')
-async def cancelreminder(ctx, reminder_id: str):
+async def cancelreminder(ctx, reminder_id: int):
     if reminder_id in reminders:
         del reminders[reminder_id]
-        await ctx.send(f'{ctx.author.mention}, your reminder with ID `{reminder_id}` has been canceled.')
+        await ctx.send(f'{ctx.author.mention}, your reminder with ID [**{reminder_id}**](https://dummy-url/{reminder_id}) has been canceled.')
     else:
-        await ctx.send(f'{ctx.author.mention}, no reminder found with ID `{reminder_id}`.')
+        await ctx.send(f'{ctx.author.mention}, no reminder found with ID [**{reminder_id}**](https://dummy-url/{reminder_id}).')
 
 # Run the bot
 bot.run(DISCORD_TOKEN)
